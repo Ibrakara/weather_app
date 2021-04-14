0a574a9defeb430cc2f92dbb659058e7514b4c53
Ibrakara <ibr.karaduman@gmail.com>
2021-04-14 11:41:18 +0300

    Remove api console data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import "./style.css";
-import apiCall from "./api";
 import { bodyRender } from "./elements/body";
 import headerRender from "./elements/header";
 import getCity from "./search";
 import getCurrentCityWeather from "./currentCityWeather";
 
-apiCall("Ankara").then((data) => console.log(data));
 headerRender();
 bodyRender();
 getCity();
