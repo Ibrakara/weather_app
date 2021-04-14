62f15b88dea32edc63a625ef5001f30365a78f4b
Ibrakara <ibr.karaduman@gmail.com>
2021-04-14 13:29:37 +0300

    Change ip api with a secure one

--- src/currentCityWeather.js
+++ src/currentCityWeather.js
@@ -2,17 +2,15 @@ import apiCall from "./api";
 import { currentWheatherRender, hourlyForecastRender } from "./elements/body";
 
 const getCurrentCityWeather = async function () {
-  const fetchIp = await fetch("https://api.ipify.org?format=json", {
+  // const fetchIp = await fetch("https://api.ipify.org?format=json", {
+  //   mode: "cors",
+  // });
+  // const ipData = await fetchIp.json();
+  const fetcCurrentLocationData = await fetch("https://ipapi.co/json/", {
     mode: "cors",
   });
-  const ipData = await fetchIp.json();
-  const fetcCurrentLocationData = await fetch(
-    `http://ip-api.com/json/${ipData.ip}`,
-    {
-      mode: "cors",
-    }
-  );
   const cityData = await fetcCurrentLocationData.json();
+  // console.log(cityData);
   apiCall(cityData.city)
     .then((data) => {
       currentWheatherRender(data);
